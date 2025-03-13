cd59b04df5aaa1725c977588ae9687b8ce1b3dbd
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2025-03-13 04:40:18 +0200

    custom text field

---
+++ lib/Views/Auth/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:dyslexia_app/Core/Constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: kveryWhite,
+      body: Column(
+        
+      ),
+    );
+  }
+}
