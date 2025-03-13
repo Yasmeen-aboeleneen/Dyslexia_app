cc2cb28cf5ebcc774ede7ece2e159f8344c81eb8
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2025-03-13 23:24:26 +0200

    SharedPreferencesSingleton

--- lib/Views/Auth/login_screen.dart
+++ lib/Views/Auth/login_screen.dart
@@ -1,9 +1,15 @@
 import 'package:dyslexia_app/Core/Constants/colors.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+static const String loginRoute = 'login';
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: '',
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashScreen.routeName,
-      home: SplashScreen(),
-      
-     );
+    );
   }
 }
-
- 
