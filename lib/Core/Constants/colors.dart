7816450aaaa5f1d1d1b44c9d80959722221dd17e
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2025-03-18 05:27:06 +0200

    Login Screen

--- lib/Core/Constants/colors.dart
+++ lib/Core/Constants/colors.dart
@@ -6,12 +6,13 @@ const Color kPrimary2 = Color.fromARGB(255, 142, 84, 38);
 const Color kPrimary3 = Color.fromARGB(255, 220, 118, 127);
 const Color kPrimary4 = Color.fromARGB(255, 244, 197, 159);
 
-
 const Color kRed = Color.fromARGB(255, 154, 40, 32);
 const kveryWhite = Color.fromARGB(255, 255, 255, 255);
 const Color kLightPurple = Color.fromARGB(255, 231, 212, 234);
 
 const Color kGrey = Color.fromARGB(255, 225, 224, 225);
+const Color kMGrey = Color.fromARGB(255, 192, 190, 190);
+
 const Color kDGrey = Color.fromARGB(255, 94, 93, 94);
 const Color kwarm = Color.fromARGB(255, 238, 243, 218);
 const Color kBlue = Color.fromARGB(255, 12, 78, 132);
--- lib/Views/Auth/login_screen.dart
+++ lib/Views/Auth/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:dyslexia_app/Core/Constants/colors.dart';
+import 'package:dyslexia_app/Core/Widgets/custom_app_bar.dart';
+import 'package:dyslexia_app/Views/Auth/Widgets/login_screen_body.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,11 +14,12 @@ static const String loginRoute = 'login';
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+     var w = MediaQuery.of(context).size.width;
+    // var h = MediaQuery.of(context).size.height;
+    return   Scaffold(
+      appBar: BuildAppBar(context, w, title: 'تسجيل الدخول'),
       backgroundColor: kveryWhite,
-      body: Column(
-        
-      ),
+      body:  LoginScreenBody()
     );
   }
 }
